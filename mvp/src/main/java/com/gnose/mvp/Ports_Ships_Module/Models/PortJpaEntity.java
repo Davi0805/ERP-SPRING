7b94f3236d2d76870ac1347e5cbac2ee896df566
Davi0805 <melocardozo.video@gmail.com>
2025-04-15 12:28:21 +0100

    feat(Ports_Ships): more jpa entities added

---
+++ mvp/src/main/java/com/gnose/mvp/Ports_Ships_Module/Models/PortJpaEntity.java
@@ -0,0 +1,28 @@
+package com.gnose.mvp.Ports_Ships_Module.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ports")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PortJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(unique = true)
+    private String code;
+}
---
+++ mvp/src/main/java/com/gnose/mvp/Ports_Ships_Module/Models/ShipsJpaEntity.java
@@ -0,0 +1,27 @@
+package com.gnose.mvp.Ports_Ships_Module.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ShipsJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String imo_number; // international ship unique identifier
+
+    private Integer capacity; // in TEU (Twenty-foot Equivalent Unit)
+}
