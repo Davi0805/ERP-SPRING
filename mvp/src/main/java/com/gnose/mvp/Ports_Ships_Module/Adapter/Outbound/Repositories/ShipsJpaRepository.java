8765066c35569d37c7a64a2a7029bfc9add3f1f6
Davi0805 <melocardozo.video@gmail.com>
2025-04-20 13:18:47 +0100

    fix(ShipsJpaRepo): wrong method name

--- mvp/src/main/java/com/gnose/mvp/Ports_Ships_Module/Adapter/Outbound/Repositories/ShipsJpaRepository.java
+++ mvp/src/main/java/com/gnose/mvp/Ports_Ships_Module/Adapter/Outbound/Repositories/ShipsJpaRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ShipsJpaRepository extends JpaRepository<ShipsJpaEntity, Long> {
-    Optional<ShipsJpaEntity> findByImoNumber(String imo_number);
+    //Optional<ShipsJpaEntity> findByImo_number(String imo_number);
 }
