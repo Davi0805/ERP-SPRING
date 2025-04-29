8efd7cf45cebc480d5cb4dbae3abafe430c2bd7a
Davi0805 <melocardozo.video@gmail.com>
2025-04-29 14:57:04 +0100

    refac(kafka): fuck kafka jsondesserializer, im gonna deserialize on hand. Later, i read the doc to clean it

--- mvp/src/main/java/com/gnose/mvp/Kafka/KafkaConsumerConfig.java
+++ mvp/src/main/java/com/gnose/mvp/Kafka/KafkaConsumerConfig.java
@@ -1,6 +1,5 @@
 package com.gnose.mvp.Kafka;
 
-import com.gnose.mvp.Notifications_Module.Infrastructure.Entities.NotificationJpaEntity;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -8,11 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,42 +15,19 @@ import java.util.Map;
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, NotificationJpaEntity> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory()
+    {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-
-        // ErrorHandlingDeserializer for both key and value
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-
-        // Inner deserializer
-        config.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
-        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-
-        // JsonDeserializer settings
-        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.gnose.mvp.Notifications_Module.Infrastructure.Entities.NotificationJpaEntity");
-        //config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.gnose.mvp.model");
-        config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
-
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
-    public RetryTemplate retryTemplate() {
-        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
-        retryPolicy.setMaxAttempts(3);
-        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
-        backOffPolicy.setBackOffPeriod(8000);
-        RetryTemplate retryTemplate = new RetryTemplate();
-        retryTemplate.setRetryPolicy(retryPolicy);
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-        return retryTemplate;
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, NotificationJpaEntity> concurrentKafkaListenerContainerFactory()
+    public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory()
     {
-        ConcurrentKafkaListenerContainerFactory<String, NotificationJpaEntity> listener = new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, String> listener = new ConcurrentKafkaListenerContainerFactory<>();
         listener.setConsumerFactory(consumerFactory());
         return listener;
     }
