7d45f72f91a4bfc7d2e743f29e1103fb7e8ea5e2
Davi0805 <melocardozo.video@gmail.com>
2025-05-18 20:11:31 +0100

    feat(Authorization): BaseController Abstract class created to implement methods to get authorizations from request context

---
+++ mvp/src/main/java/com/gnose/mvp/Authorization/AuthorizationBaseController.java
@@ -0,0 +1,25 @@
+package com.gnose.mvp.Authorization;
+
+import com.gnose.mvp.Core.Adapter.outbound.DTO.CompanyPermissionDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public abstract class AuthorizationBaseController {
+    protected HttpServletRequest request;
+
+    @Autowired
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected List<CompanyPermissionDTO> getAuthorizedCompanies() {
+        return (List<CompanyPermissionDTO>) request.getAttribute("authorizedCompanies");
+    }
+
+    protected Long getCompanyId() {
+        return (Long) request.getAttribute("companyId");
+    }
+}
